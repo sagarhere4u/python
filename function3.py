513a04f7c0b242214fb14f62f11f5c415d75c838
Sagar Mehta <sagar.mehta@atgensoft.com>
2025-05-14 21:29:25 +0530

    adding function3.py

---
+++ function3.py
@@ -0,0 +1,11 @@
+#function3.py
+
+def printinfo(name, age=35):
+    print ("Name: ", name)
+    print("Age:", age)
+    return
+
+printinfo(age=25, name="john")
+printinfo(name="stuart")
+printinfo("john")
+printinfo("matt",22)
