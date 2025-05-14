90d6b263cfccb136ff597dc214facc3db88c2318
Sagar Mehta <sagar.mehta@atgensoft.com>
2025-05-14 21:03:46 +0530

    adding function1.py

---
+++ function1.py
@@ -0,0 +1,11 @@
+#function1.py
+
+#define a function
+def printme(var):
+    print("Var is: ", var)
+    return
+
+#call a function
+printme("This is sample var value")
+printme(10)
+print("GoodBye!")
