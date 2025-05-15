c5e21696a844069ad349901c881f96072810a394
Sagar Mehta <sagar.mehta@atgensoft.com>
2025-05-15 19:10:41 +0530

    adding fileappend.py

---
+++ fileappend.py
@@ -0,0 +1,8 @@
+#fileappend.py
+
+f = open("myfile.txt", "a")
+f.write("This is line number 5\n")
+f.write("This is line number 6\n")
+f.write("This is line number 7\n")
+f.write("This is line number 8\n")
+f.close()
