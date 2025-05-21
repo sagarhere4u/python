8afc454e0bc3556ec64cec154dafa25bea5b144c
Sagar Mehta <sagar.mehta@atgensoft.com>
2025-05-21 22:15:53 +0530

    add pandas11

---
+++ pandas11.py
@@ -0,0 +1,18 @@
+#pandas11.py
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv('pandasdata.csv')
+
+for x in df.index:
+  if df.loc[x, "Income"] < 5000:
+    df.drop(x, inplace=True)
+
+ax = df['Sex'].value_counts()
+ax.plot.bar(color=["orange", "blue"], width=.3)
+
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
